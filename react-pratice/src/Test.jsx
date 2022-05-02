0b24d3efa0b7a6e3ade38ef4d7dd6bbbecc81aff
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-05-03 07:57:55 +0900

    feat: 상위 컴포넌트에서 하위 컴포넌트의 DOM노드에 접근

--- react-pratice/src/Test.jsx
+++ react-pratice/src/Test.jsx
@@ -1,5 +1,30 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React, { useRef, forwardRef } from 'react';
+
 function Test() {
-  return <div>Test!</div>;
+  return <MyForm />;
+}
+
+const MyInput = forwardRef((props, ref) => {
+  return <input {...props} ref={ref} />;
+});
+
+function MyForm() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <>
+      <MyInput ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
+    </>
+  );
 }
 
 export default Test;
